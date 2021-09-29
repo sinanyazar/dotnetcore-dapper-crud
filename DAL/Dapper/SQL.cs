483b7180315cd21255ffff19c554f2ef47e43a9c
sinanyazar <48910244+sinanyazar@users.noreply.github.com>
2021-09-29 18:58:56 +0300

    SQL class added.

--- DAL/Dapper/SQL.cs
+++ DAL/Dapper/SQL.cs
@@ -1,12 +1,31 @@
-// using System.Data.SqlClient;
-// using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace DAL.Dapper
 {
-    public class SQL
+    public class SQL<T>
     {
-        // private SqlConnection _connection;
+        private SqlConnection _connection;
 
-        // private const string cnn = @"Data Source=(local);Initial Catalog=AdventureWorks2017;User ID=sa;Password=123";
+        private const string cnn = @"Data Source=(local);Initial Catalog=AdventureWorks2017;User ID=sa;Password=123";
+
+        public IEnumerable<T> Query(string query)
+        {
+            using(_connection = new SqlConnection(cnn))
+            {
+                var result = _connection.Query<T>(query);
+
+                return result;
+            }
+        }
+
+        public void Execute(string query)
+        {
+            using(_connection = new SqlConnection(cnn))
+            {
+                _connection.Execute(query);
+            }
+        }
     }
 }
